58452b53ec90bd99f030910b1ee6ca86ddc660f9
ty61271 <40565834+ty61271@users.noreply.github.com>
2018-12-18 21:14:50 +0800

    Initial commit

---
+++ src/main/kotlin/com/love/Hello.kt
@@ -0,0 +1,14 @@
+package com.love
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
