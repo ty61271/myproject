8e2c40806cf7096decf9e1cfaeaee0c839b5e8e8
ty61271 <40565834+ty61271@users.noreply.github.com>
2018-12-18 21:42:50 +0800

    Java and Kotlin variable,data type

--- src/main/kotlin/com/love/Hello.kt
+++ src/main/kotlin/com/love/Hello.kt
@@ -3,12 +3,21 @@ package com.love
 fun main(args: Array<String>) {
 //    println("Hello Kotlin")
 //    Human().hello()
-    val h=Human()
+
+//    val 不能改變資料
+    val h = Human()
     h.hello()
+//    var 可變資料
+    var age = 19
+    age = 20
+    var weight = 66.5f
+//    先定義後給值
+    var name: String
+    name = "West"
 }
 
 class Human {
-    fun hello(){
+    fun hello() {
         println("Hello kotlin")
     }
 }
