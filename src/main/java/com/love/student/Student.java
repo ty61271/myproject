633f1877bbb2365ab941698647f030a8bba9e2fb
ty61271 <40565834+ty61271@users.noreply.github.com>
2018-12-20 23:24:45 +0800

    ch18

--- src/main/java/com/love/student/Student.java
+++ src/main/java/com/love/student/Student.java
@@ -18,13 +18,33 @@ public class Student {
     }
 
     public void print() {
+        int average=getAverage();
         System.out.print(name + "\t" + english + "\t" + math + "\t"
-                + getAverage());
-        if(getAverage()>=60){
-            System.out.println("\tPass");
-        }else {
-            System.out.println("\tFailed");
+                + getAverage() + "\t" + ((getAverage()>=60) ? "Pass":"FAILED"));
+        char grading='F';
+        switch (average/10){
+            case 10:
+            case 9:
+                grading='A';
+                break;
+            case 8:
+                grading='B';
+                break;
+            case 7:
+                grading='C';
+                break;
+            case 6:
+                grading='D';
+                break;
+                default:
+                    grading='F';
         }
+        System.out.println("\t"+grading);
+//        if(getAverage()>=60){
+//            System.out.println("\tPass");
+//        }else {
+//            System.out.println("\tFailed");
+//        }
     }
     public int getAverage(){
         return (english + math) / 2;
