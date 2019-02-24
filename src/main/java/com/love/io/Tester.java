f670a9f6f97e642c3bd6ba669126a651f394b8dc
ty61271 <40565834+ty61271@users.noreply.github.com>
2019-02-24 18:08:55 +0800

    ch34

--- src/main/java/com/love/io/Tester.java
+++ src/main/java/com/love/io/Tester.java
@@ -1,19 +1,56 @@
 package com.love.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 public class Tester {
     public static void main(String[] args) {
+        try {
+            BufferedReader br=new BufferedReader(new FileReader("data.txt"));
+            String line=br.readLine();
+            while (line!=null){
+                System.out.println(line);
+                line=br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        filereader();
+//        inputsream();
+    }
+
+    private static void filereader() {
+        try {
+            FileReader fr=new FileReader("data.txt");
+            int n=fr.read();
+            while (n!=-1){
+                System.out.print((char) n);
+                n=fr.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void inputsream() {
         File file=new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
         try {
             FileInputStream is=new FileInputStream(file);
-        }catch (FileNotFoundException e){
+            int n =is.read();
+            while (n!=-1){
+                System.out.print((char) n);
+                n=is.read();
+            }
+            System.out.println();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.out.println("message");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println("File open success");
     }
